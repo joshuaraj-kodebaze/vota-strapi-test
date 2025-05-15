fc334443f60706ffdf5d172a06448931133154fc
Joshua Raj <joshua.raj@kodebaze.com>
2025-05-15 17:27:21 +0800

    revoke

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-supabase",
-      providerOptions: {
-        apiUrl: env("SUPABASE_API_URL"),
-        apiKey: env("SUPABASE_API_KEY"),
-        bucket: env("SUPABASE_BUCKET"),
-        directory: env("SUPABASE_DIRECTORY", ""),
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     provider: "strapi-provider-upload-supabase",
+  //     providerOptions: {
+  //       apiUrl: env("SUPABASE_API_URL"),
+  //       apiKey: env("SUPABASE_API_KEY"),
+  //       bucket: env("SUPABASE_BUCKET"),
+  //       directory: env("SUPABASE_DIRECTORY", ""),
+  //     },
+  //   },
+  // },
 });
